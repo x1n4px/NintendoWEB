8621a1b15de07833f0377284708be4e6ac5de9d9
x1n4px <silosabia@gmail.com>
2023-07-16 14:25:45 +0200

    Home

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductosService } from 'src/app/service/productos.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  listaJuegos: any[] = [];
+  x: number = 0;
+  constructor(private productService: ProductosService) {
+    this.listaJuegos = productService.devolverLista();
+  }
 
+
+  moverSlide(position: number) {
+     if (position === 0) {
+      if (this.x > 0) {
+        this.x -= 4;
+
+      }
+    } else if (position === 1) {
+      this.x += 4;
+
+    }
+    console.log(this.x);
+  }
 }
--- src/app/service/productos.service.ts
+++ src/app/service/productos.service.ts
@@ -6,4 +6,38 @@ import { Injectable } from '@angular/core';
 export class ProductosService {
 
   constructor() { }
+
+  juegosPortadas = [
+    {
+      image: './assets/portadas/pikmin4Portada.png',
+      name: 'pikmin 4',
+      description: 'Get double the points*-Offer ends 7/23',
+      console: 'Nintendo Switch'
+    },{
+      image: './assets/portadas/pikmin1Portada.png',
+      name: 'pikmin 1',
+      description: 'Pimin 1+2-Available now',
+      console: 'Nintendo Switch'
+    },{
+      image: './assets/portadas/zeldaPortada.png',
+      name: 'Zeld',
+      description: 'Tears of the kingdom-Available now',
+      console: 'Nintendo Switch'
+    },{
+      image: './assets/portadas/mariokart8Portda.png',
+      name: 'Mario Kart 8',
+      description: 'Mario Kart 8 Deluxe DLC Wave 5-Available now',
+      console: 'Nintendo Switch'
+    },{
+      image: './assets/portadas/everybodySwitchPortada.png',
+      name: 'EveryBody Switch',
+      description: 'Everybody 1-2-Switch!-Available now',
+      console: 'Nintendo Switch'
+    }
+  ];
+
+
+  devolverLista() {
+    return this.juegosPortadas;
+  }
 }
